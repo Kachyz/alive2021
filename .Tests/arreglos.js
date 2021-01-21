184cc85ed508b7ad05dc1fc7410d4d604703d72e
CarlosEstrada <kachiiz@gmail.com>
2021-01-21 00:41:19 -0600

    un par de ejercicios de arreglos

--- .Tests/arreglos.js
+++ .Tests/arreglos.js
@@ -1,12 +1,21 @@
 const ej01 = require('../02_Arreglos/01')
+const ej02 = require('../02_Arreglos/02')
+const ej03 = require('../02_Arreglos/03')
 
 const GOOD = 'PASSED'
 const BAD = '== ERROR =='
 let results = [];
 
-// 01
-results.push( ej01.length === 10 ? GOOD : BAD )
+const checkEquals = (received, expected ) => received === expected ? GOOD : BAD
 
+// 01
+results.push( checkEquals(ej01, 10) )
+
+// 02
+results.push( checkEquals(ej02, 'flan') )
+
+// 03
+results.push( checkEquals(ej03, 395) )
 
 
 for ( i in results) {
---
+++ 02_Arreglos/02.js
@@ -0,0 +1,11 @@
+// Asigna a la variable llamada 'postreFavorito' el primer elemento del arreglo 'todosLosPostres' 
+
+let todosLosPostres = ['flan', 'pastel', 'jericalla', 'gelatina', 'carlota'];
+let postreFavorito
+
+// Inicio de tu código
+
+
+// Fin de tu código
+
+module.exports = postreFavorito;
