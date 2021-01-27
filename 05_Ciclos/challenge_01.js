8a995addebf611ae663873a8bdfa733f03479e79
CarlosEstrada <kachiiz@gmail.com>
2021-01-27 00:07:28 -0600

    Propuesta de challenges

---
+++ 05_Ciclos/challenge_01.js
@@ -0,0 +1,8 @@
+// Dado la variable 'numeros' regresa la cantidad de 1 que se almacenan en la misma
+
+const numeros = [
+  [1,0,1,1,0,1,0,1,1,0,0,0,1],
+  [1,0,1,1,0,1,0,1,1,0,0,1,1,1,1,0,0,0,1,0,1,0,0],
+  [1,0,0,0,0,0,0,1,0,0,0,1],
+  [1,1,1,1,1,1,1,0,0,1,1,0,1]
+]
---
+++ 06_Funciones/challenge_01.js
@@ -0,0 +1,7 @@
+// Dada la cadena de texto 'trabalenguas', cuenta la cantidad de veces que cada letra aparece
+// NOTAS
+// * Se removieron los acentos para facilitar el ejercicio
+// * No queremos contar espacios, comas o puntos.
+// * No se busca hacer distinción entre mayúsculas y minúsculas 
+
+const trabalenguas = "Si tu gusto no gusta del gusto que gusta mi gusto, que disgusto se lleva mi gusto al saber que tu gusto no gusta del gusto que gusta mi gusto."
