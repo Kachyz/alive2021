9b1e13cd3177e6f7d658cbbf5f2f0f78d7363106
damarisrmariscal@gmail.com <damarisrmariscal@gmail.com>
2021-01-28 17:44:17 -0600

    Variables

--- 01_Variables/01.js
+++ 01_Variables/01.js
@@ -1,5 +1,8 @@
 // Crea una variable que contenga la frase 'Bienvenidos a ALIVE 2021' e imprimela utilizando un console.log
 
 //Saludo
-let frase = "Bienvenidos a ALIVE 2021";
-console.log (frase);
+(function variables() {
+    // Tu código va aquí
+    let frase = "Bienvenidos a ALIVE 2021";
+    console.log (frase);
+  })()
--- 01_Variables/04.js
+++ 01_Variables/04.js
@@ -1,7 +1,10 @@
 //Mostrar el valor en un console del area de un circulo
 
 //Fórmula circulo
-const PI = 3.14
-const RADIO = 2
-let area = (RADIO**2)* PI
-console.log (area)
+(function variables() {
+    //Tu código va aquí
+    const PI = 3.14
+    const RADIO = 2
+    let area = (RADIO**2)* PI
+    console.log ("Resultado área círculo con radio de dos es: " , area)
+})()
