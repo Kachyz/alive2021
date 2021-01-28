7fdd769ed929c77002e51f98dad4f9dc966c47e2
Hiromi Akita <hiromiakitao@gmail.com>
2021-01-28 15:29:41 -0600

    arreglos - ejercicio 4

---
+++ 02_Arreglos/04.js
@@ -0,0 +1,11 @@
+// Del arreglo arregloUsuarios, sustituye a Julio por Luis 
+
+let arregloUsuarios = ['Diego', 'Julio', 'Ana', 'Paula'];
+
+  
+  // Inicio de tu código
+  
+  
+  // Fin de tu código
+  
+module.exports = arregloUsuarios;
