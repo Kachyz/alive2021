ae653498ba705d27d855881d09690b6fbead9cb0
dianaatorress <dianaatorress@users.noreply.github.com>
2021-01-28 17:54:26 -0600

    mas cambios

--- 01_Variables/03.js
+++ 01_Variables/03.js
@@ -1,10 +1,12 @@
 // Crea una nueva cadena de texto que una las variables listadas y una coma intermedia
 
 (function variables() {
-  const saludo = 'Hola'
-  const pregunta = 'cómo estás?'
+  const saludo = 'Hola';
+  const pregunta = 'cómo estás?';
 
   // Tu código va aquí
-  console.log(saludo, ",", pregunta);
+  listo = saludo + ", " + pregunta
+  console.log(listo);
+  // console.log(saludo,",", pregunta);
 
 })()
