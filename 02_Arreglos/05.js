eaeb1f5e74bb99b2f3d5969c79e82896b3138f4a
Hiromi Akita <hiromiakitao@gmail.com>
2021-01-28 15:49:33 -0600

    arreglos - ejercicio 5

---
+++ 02_Arreglos/05.js
@@ -0,0 +1,10 @@
+// Guarda en la variable suma la suma de los tres números del arreglo numeros
+let numeros = [12, 4, 8];
+
+let suma
+// Inicio de tu código
+
+
+// Fin de tu código
+
+module.exports = suma;
