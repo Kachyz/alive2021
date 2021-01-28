2a7b4747cc7eab0dd33f796325bb191ed3ab92e7
damarisrmariscal@gmail.com <damarisrmariscal@gmail.com>
2021-01-28 16:52:56 -0600

    Variables

--- 01_Variables/01.js
+++ 01_Variables/01.js
@@ -1,6 +1,5 @@
 // Crea una variable que contenga la frase 'Bienvenidos a ALIVE 2021' e imprimela utilizando un console.log
 
-(function variables() {
-  // Tu código va aquí
-
-})()
+//Saludo
+let frase = "Bienvenidos a ALIVE 2021";
+console.log (frase);
--- 01_Variables/02.js
+++ 01_Variables/02.js
@@ -1,6 +1,5 @@
 // Crea una variable que almacene el resultado de la operación  23 al cubo
 
-(function variables() {
-  // Tu código va aquí
-
-})()
+//23 al cubo
+let exponenteResultado = 23**3;
+console.log ("23 al cubo es" , exponenteResultado);
