55f6fd970d830cc0d56a8a6ec190441de7870b21
Regina-a <gomezpr.2005@gmail.com>
2021-01-28 17:05:40 -0600

    lllll

--- 01_Variables/01.js
+++ 01_Variables/01.js
@@ -1,6 +1,4 @@
 // Crea una variable que contenga la frase 'Bienvenidos a ALIVE 2021' e imprimela utilizando un console.log
 
-(function variables() {
-  // Tu código va aquí
-
-})()
+let hola = "bienvenidos a ALIVE";
+console.log( hola)
--- 01_Variables/02.js
+++ 01_Variables/02.js
@@ -1,6 +1,4 @@
 // Crea una variable que almacene el resultado de la operación  23 al cubo
 
-(function variables() {
-  // Tu código va aquí
-
-})()
+let operacion = 23*23*23;
+console.log(operacion);
