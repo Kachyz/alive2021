d12c20add50120b88bf1aea7bdabb4c57e1bf644
--global <>
2021-01-28 17:10:51 -0600

    ejercicio1listo

--- 01_Variables/01.js
+++ 01_Variables/01.js
@@ -1,6 +1,7 @@
 // Crea una variable que contenga la frase 'Bienvenidos a ALIVE 2021' e imprimela utilizando un console.log
+let saludo = "Bienvenidos a ALIVE 2021"
 
 (function variables() {
   // Tu código va aquí
-
+    console.log(saludo);
 })()
