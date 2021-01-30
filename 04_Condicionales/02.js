3a8555dfd4ed3fccbe5faf45708fcec2f16f5d5d
CarlosEstrada <kachiiz@gmail.com>
2021-01-29 18:13:40 -0600

    Ejercicios de condicionales y funciones

---
+++ 04_Condicionales/02.js
@@ -0,0 +1,4 @@
+// Dado el arreglo 'letras' revisemos si contiene la letra 'Z' y de ser así impriome en pantalla el mensaje "LETRA ENCONTRADA"
+// De lo contrario, agregala al conjunto de letras e imprime tanto el arreglo resultante como el mensaje 'LETRAS AÑADIDA'
+
+let letras = ['A', 'K', 'W', 'O', 'R']
---
+++ 06_Funciones/05.js
@@ -0,0 +1,2 @@
+// Crea una funcion que reciba 2 numeros y regresa la división del primero sobre el segundo
+// Valida que la operación pueda ser procesada, de lo contrario regresa un mensaje de error
