5b4fc637a527bc866bf15834268851c5fc69aea4
dianaatorress <dianaatorress@users.noreply.github.com>
2021-02-02 19:14:01 -0600

    ejercicios

--- 02_Arreglos/01.js
+++ 02_Arreglos/01.js
@@ -1,10 +1,11 @@
 // Agrega al arreglo "nuevoArreglo" 10 elementos
 
-let nuevoArreglo = [];
-
+let nuevoArreglo = ["Hola","soy","Diana","Alejandra","alias","Adhara","que","tal","tu","como","estas"];
+// console.table(nuevoArreglo);
 // Inicio de tu código
+console.log(nuevoArreglo);
 
 
 // Fin de tu código
 
-module.exports = nuevoArreglo;
+// module.exports = nuevoArreglo;
