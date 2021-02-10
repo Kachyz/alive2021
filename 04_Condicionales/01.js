0ceb10b3252227a7241aaa4286a4118f12d7a3ea
dianaatorress <dianaatorress@users.noreply.github.com>
2021-02-09 18:51:08 -0600

    ejercicios de condicionales

--- 04_Condicionales/01.js
+++ 04_Condicionales/01.js
@@ -1,1 +1,24 @@
-// Dada una variable color, revisa si es ROJO o AZUL e imprime el color resultante de su combinacion con AMARILLO
+// // Dada una variable color, revisa si es ROJO o AZUL e imprime el color resultante de su combinacion con AMARILLO
+// let color = "AZUL";
+// // let combinacion = "AMARILLO";
+//
+// if(color == "ROJO"){
+//   result = "naranja";
+//   console.log("La combinación del color ",color,"con el amarillo es ",result);
+// }else if (color == "AZUL") {
+//   result = "verde";
+//   console.log("La combinación del color ",color,"con el amarillo es ",result);
+// } else {
+//   console.log("Esto es incorrecto");
+// }
+
+let color = "ROJO";
+
+if(color == "ROJO"){
+  result = "naranja";
+  console.log("La combinación del color ",color,"con el amarillo es ",result);
+}
+if (color == "AZUL") {
+  result = "verde";
+  console.log("La combinación del color ",color,"con el amarillo es ",result);
+}
--- 04_Condicionales/02.js
+++ 04_Condicionales/02.js
@@ -1,4 +1,26 @@
 // Dado el arreglo 'letras' revisemos si contiene la letra 'Z' y de ser así impriome en pantalla el mensaje "LETRA ENCONTRADA"
 // De lo contrario, agregala al conjunto de letras e imprime tanto el arreglo resultante como el mensaje 'LETRAS AÑADIDA'
 
-let letras = ['A', 'K', 'W', 'O', 'R']
+let letras = ['A', 'K', 'W', 'O', 'R'];
+
+// zeta = letras.index('Z');
+//
+// if (zeta == true) {
+//   console.log("LETRA ENCONTRADA");
+// }
+//
+// if (zeta == false) {
+//   letras.append('z');
+//   console.log(letras);
+//   console.log("LETRA AÑADIDA");
+// }
+
+if ( (letras[0] == 'Z') || (letras[1] == 'Z') || (letras[2] == 'Z') || (letras[3] == 'Z') || (letras[4] == 'Z') ) {
+  console.log("LETRA ENCONTRADA");
+}else {
+  letras = ['A','K','W','O','R','Z'];
+  // letras[6] = 'F';
+  console.log(letras);
+  console.log("LETRA AÑADIDA");
+
+}
