b563b49bfabe5ff3d8dd030e241bb10683b4e4e8
Regina-a <gomezpr.2005@gmail.com>
2021-02-11 17:10:21 -0600

    q

--- 04_Condicionales/01.js
+++ 04_Condicionales/01.js
@@ -1,1 +1,16 @@
 // Dada una variable color, revisa si es ROJO o AZUL e imprime el color resultante de su combinacion con AMARILLO
+
+let rojo= true;
+let  azul = true;
+
+ 
+if (rojo == false){
+console.log("naranja");
+
+}else { ( azul == true)  
+console.log("verde");
+
+}
+
+
+
---
+++ 04_Condicionales/02.js
@@ -0,0 +1,27 @@
+// Dado el arreglo 'letras' revisemos si contiene la letra 'Z' y de ser así imprime en pantalla el mensaje "LETRA ENCONTRADA"
+// De lo contrario, agregala al conjunto de letras e imprime tanto el arreglo resultante como el mensaje 'LETRAS AÑADIDA'
+
+let letras = ['A', 'K', 'W', 'O', 'R'];
+let zeta= 'Z'
+
+if (letras[0] = 'A'){
+console.log("letra encontrada")
+}
+
+
+
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+
+
+
+
+
+
+
+
+
+
+//if (letras = letras.find(element => element > 'Z'))
+//console.log("letra encontrada")
+
