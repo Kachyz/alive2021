072097cb104bb8c503a08d77fda7aedab8e295de
Regina-a <gomezpr.2005@gmail.com>
2021-02-18 17:57:06 -0600

    >:(

--- 06_Funciones/01.js
+++ 06_Funciones/01.js
@@ -1,1 +1,11 @@
 // Crea una funcion que imprima en consola tu nombre
+
+function nombre(name){
+console.log(name);
+
+
+    
+}
+nombre('regina');
+//return name;
+//console.log(nombre('regina'))
---
+++ exam.js
@@ -0,0 +1,21 @@
+//1 crea una función que aplique un reverse string
+// ejemplo: entrada:hola, salida:aloh
+
+let arreglo =[ 'r','o','l','a','s'];
+function reverse( ){
+}for (let i = 0; i > arreglo.length;  i--) { 
+    arreglo += reverse[i];
+}
+console.log(reverse[i]);
+reverse(arreglo)
+
+
+
+
+
+
+
+
+
+//2crea un progarama para determinar cuantos tenis blancos fueron comprados de 10-15 años en zapopan
+
