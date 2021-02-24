fafe49a7ea27bdb975f74eae2c6aff1cdb9612b0
dianaatorress <dianaatorress@users.noreply.github.com>
2021-02-23 18:51:32 -0600

    ejercicios ciclos

--- 05_Ciclos/01.js
+++ 05_Ciclos/01.js
@@ -1,10 +1,24 @@
 // Utiliza un ciclo para encontrar la suma de todos los numeros en el rango de 0 a 200
 
-let resultado
-
-// Inicio de tu código
-
+let resultado = 0;
+//
+// // Inicio de tu código
+// for(let i=0; i>=0; i++) {
+//   resultado2 += i;
+//   console.log(resultado2);
+// }
 
 // Fin de tu código
 
+
+// for (i=0; i<=200; i++){
+//   resultado += i;
+//   console.log(resultado);
+// }
+let i = 0;
+while (i<= 200) {
+  resultado += i;
+  i ++;
+  console.log(resultado);
+}
 module.exports = resultado;
--- 05_Ciclos/02.js
+++ 05_Ciclos/02.js
@@ -1,1 +1,13 @@
 // Crea un ciclo que imprima un asterisco (*) 725 veces en UNA SOLA linea
+let resultado = "";
+let i = 0;
+while (i <= 725) {
+  resultado += "*";
+  i ++;
+}
+console.log(resultado);
+
+// for(let i=0; i <=725; i++){
+//   resultado += "*";
+// }
+// console.log(resultado);
