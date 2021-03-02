afd46c637a52044d85fa57e0b211f3d3162736fc
AlmaSTorresS@gmail.com <AlmaSTorresS@gmail.com>
2021-03-02 17:59:48 -0600

    cambios y reto

--- 05_Ciclos/challenge_01.js
+++ 05_Ciclos/challenge_01.js
@@ -7,4 +7,38 @@ const numeros = [
   [1,1,1,1,1,1,1,0,0,1,1,0,1]
 ]
 
-console.log("En total, aquí hay " + arregloDeUnos.length + " unos.");
+let arreglo1 = numeros[0];
+let arreglo2 = numeros[1];
+let arreglo3 = numeros[2];
+let arreglo4 = numeros[3];
+
+let unos = 0;
+for(let i = 0; i < arreglo1.length; i++) {
+  if (arreglo1[i] === 1) {
+    unos++
+  }
+}
+
+let unos1 = 0;
+for(let i = 0; i < arreglo2.length; i++) {
+  if (arreglo2[i] === 1) {
+    unos++
+  }
+}
+
+let unos2 = 0;
+for(let i = 0; i < arreglo3.length; i++) {
+  if (arreglo3[i] === 1) {
+    unos++
+  }
+}
+
+let unos3 = 0;
+for(let i = 0; i < arreglo4.length; i++) {
+  if (arreglo4[i] === 1) {
+    unos++
+  }
+}
+
+console.log("¿Cuántos números uno hay en total? ");
+console.log(unos + unos1 + unos2 + unos3);
--- 06_Funciones/02.js
+++ 06_Funciones/02.js
@@ -1,2 +1,10 @@
 // Crea una funcion que reciba un numero como 'edad' y te regrese un boleano de acuerdo a si
 // es mayor de edad o no
+
+let edad = 18;
+let dada = window.prompt("¿Cuántos años tienes?");
+if (dada >= edad) {
+  console.log("Eres mayor de edad.");
+} else if (dada < edad) {
+  console.log("Eres menor de edad.");
+}
