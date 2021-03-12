7dd27b83c3bf873c2ebc8cd0bf530a7042fb0a62
AlmaSTorresS@gmail.com <AlmaSTorresS@gmail.com>
2021-03-11 18:05:12 -0600

    funciones

--- 06_Funciones/03.js
+++ 06_Funciones/03.js
@@ -1,1 +1,18 @@
 // Crea una funcion que reciba 2 numeros y regresa la multiplicacion de los mismos
+
+let num1 = window.prompt("¿Qué número es el primer término de tu multiplicación?");
+let num2 = window.prompt("¿Qué número es el segundo término de tu multiplicación?");
+
+
+function calculo(num1, num2, operación) {
+  // operación === operación.ToLowerCase();
+
+  if (operación === 'multiplicacion') {
+    console.log("El resultado de la multiplicación es " + (num1 * num2));
+  }
+  else {
+    console.log("Error");
+  }
+}
+
+calculo(num1, num2, 'multiplicacion')
--- 06_Funciones/04.js
+++ 06_Funciones/04.js
@@ -1,2 +1,14 @@
 // Crea una funcion que reciba un numero N y una palabra
 // La funcion debe imprimir la palabra N cantidad de veces de acuerdo a lo que se especificó
+
+let palabra = window.prompt("¿Qué palabra quieres que se repita? Ingresa UNA palabra");
+let n = window.prompt("¿Cuántas veces quieres que se repita? Ingresa un NÚMERO");
+let i = 1;
+
+function pal(p, n) {
+  while (i <= n) {
+    console.log(i + ".- " + palabra);
+    i++;
+  }
+}
+pal(palabra, n);
