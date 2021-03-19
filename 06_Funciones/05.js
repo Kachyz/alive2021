7d14671439161ce5575ff079b5cc638503e40931
dianaatorress <dianaatorress@users.noreply.github.com>
2021-03-18 19:03:31 -0600

    :S cambios

--- 06_Funciones/05.js
+++ 06_Funciones/05.js
@@ -1,2 +1,16 @@
 // Crea una funcion que reciba 2 numeros y regresa la división del primero sobre el segundo
 // Valida que la operación pueda ser procesada, de lo contrario regresa un mensaje de error
+
+function division(n1,n2){
+  resultado = (n1 / n2);
+  return resultado;
+
+  if (n1 < 0) && (n2 < 0){
+    console.log("Operación inválida");
+  } else {
+
+  }
+}
+
+//ver con Hiromi eso de las operaciones invalidas
+console.log(division((parseInt(prompt("Dame un numero"))),(parseInt(prompt("Dame otro numero")))));
