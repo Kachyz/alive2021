d43cf077f37e25bb0ceecfad29c23d7ee83b7768
dianaatorress <dianaatorress@users.noreply.github.com>
2021-03-23 20:06:18 -0600

    Cambios

--- 07_DOM/01.js
+++ 07_DOM/01.js
@@ -1,1 +1,31 @@
-WIP
+// WIP
+function FuncionPrueba(){
+  let varUno = "";
+  // alert('Debuguear funcion');
+  for(i=0; i<5; i++){
+    varUno = varUno + ' ¿Por qué?'
+  }
+  document.getElementById("porquee").innerHTML = varUno;
+}
+
+// function activar(){
+//   let variableUno = '';
+//
+//     for (let i = 1; i <= 5; i++) {
+//
+//      // document.getElementById("demo").innerHTML = "porque";
+//      variableUno = variableUno + ' porque' ;
+//
+//      if(i == 3){
+//         variableUno = variableUno + ' adiós' ;
+//         document.getElementById("demo").innerHTML = "porque si";
+//      }
+//
+//     }
+//     alert(variableUno);
+//
+//   }
+
+
+// TAREA :|
+// Con una funcion y un ciclo de java script, hacer mas grande un boton, cada que se opriima el mismo
