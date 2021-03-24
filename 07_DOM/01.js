b90c23552dbc2a28607e8fbc33d925579cd180d3
damarisrmariscal@gmail.com <damarisrmariscal@gmail.com>
2021-03-23 18:03:14 -0600

    DOM

--- 07_DOM/01.js
+++ 07_DOM/01.js
@@ -1,1 +1,26 @@
-WIP
+/* function imprimirPQ(){
+    let why = "";
+
+    for (let i = 1; i <= 5; i++) {
+        why = why + '¿Por qué?'
+    }
+    alert(why)
+} */
+
+function activar(){
+    let why = '';
+    
+      for (let i = 1; i <= 5; i++) {
+       
+       // document.getElementById("demo").innerHTML = "porque";
+       why = why + '¿por qué?' ;
+       
+       //if(i == 3){
+          //variableUno = variableUno + ' adiós' ;
+          //document.getElementById("demo").innerHTML = "porque si";
+       // }
+        
+      }
+      alert(why);
+  
+    }
