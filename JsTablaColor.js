fc7718ed2aa98f358fcdbeb58b2d64ac37d638a2
Regina-a <gomezpr.2005@gmail.com>
2021-04-13 17:58:37 -0500

    r

---
+++ JsTablaColor.js
@@ -0,0 +1,14 @@
+function activar(){
+   
+        let tabla= document.getElementById('casilla');
+        tabla.style.backgroundColor= "blue";
+         console.log("s")
+        }
+function agregar(){
+    let fila= document.getElementById('casilla');
+    fila.insertRow();
+    document.getElementsByTagName(tr).innerHTML='Párrafo de texto';
+    //let palabra = document.getElementsByName('tr')
+    //palabra.document.write('texto');
+    
+}
