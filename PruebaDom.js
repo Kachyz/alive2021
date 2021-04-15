0c5a5f2e27ff37cb661e2f4fd5f3f17cee81ee86
dianaatorress <dianaatorress@users.noreply.github.com>
2021-04-15 18:57:55 -0500

    Cambios DOM

--- PruebaDom.js
+++ PruebaDom.js
@@ -1,12 +1,22 @@
 console.log("Vamos a poner color a una tabla cada vez que le piquemos a un botón");
 function cambiarColor(){
-  let cambio = document.getElementById("cambios");
-  cambio.style.backgroundColor = "#8258FA";
+  let cambio = document.getElementById("tabla");
+  cambio.style.backgroundColor = "pink";
+  cambio.style.border = "1px black solid";
+  cambio.style.color = "navy";
 }
 
 function agregarFila(){
-  let fila = document.getElementById("cambios");
-  // let nuevaFila = document.createElement("tr");
+  let tabla = document.getElementById("tabla");
+  let row = tabla.insertRow(2);
+  let cell1 = row.insertCell(0);
+  let cell2 = row.insertCell(1);
+  // cell1.innerHTML = "Nombre";
+  // cell2.innerHTML = "Apellido";
+  cell1.innerText = "Nombre";
+  cell2.innerText = "Apellido";
+
+
   // nuevaFila.appendChild("Hola");
   // fila.insertRow();
 }
