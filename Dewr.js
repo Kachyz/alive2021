532adcbae95cd7772e22fd29df8d1238575c8ca7
AlmaSTorresS@gmail.com <AlmaSTorresS@gmail.com>
2021-06-01 18:59:13 -0500

    avances

---
+++ Dewr.js
@@ -0,0 +1,9 @@
+function perso() {
+ let modal = document.getElementById("persModal");
+ modal.style.display = "block";
+}
+
+function ocultarPers() {
+ let modal = document.getElementById("persModal");
+ modal.style.display = "none";
+}
