d92e2d5d27ec1a346a69d649c094e97049114b77
--global <>
2021-06-17 15:05:57 -0500

    -subido-

---
+++ paginaInicio.js
@@ -0,0 +1,18 @@
+function comienza() {
+  let hora = document.getElementById('horas').value; //traigo las horas que ingreso el usuario
+  let minutos = document.getElementById('minutos').value; //me traigo los minutos que ingreso el usuario
+  let segundos = document.getElementById('segundos').value; //me traigo los segundos que ingreso el usuario
+  let alive = document.getElementById('alive'); //me treigo la playlist
+  let seg = 1000*segundos; //convierto los segundos ingresados a milisegundos
+  let min = 6000*minutos;//convierto los minutos ingresados a milisegundos
+  let hrs = 36000*hora;//convierto las horas ingresadas a milisegundos
+  let total = hrs + min + seg;  //sumo todo en milisegunods
+  alive.play();
+  console.log(total); //esto es solo una prueba para verificar que este bien vinculado
+
+  setTimeout(function(){ alive.play(); }, total); //funcion que cuanta los milisegundos para ejecutar la playlist
+  // hora.value = ' '; //limpia la casilla
+  // minutos.value = ' '; //limpia la casilla
+  // segundos.vale = ' '; //limpia la casilla
+
+}
